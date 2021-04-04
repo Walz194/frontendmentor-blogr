c8bfb9b7ac43e080ec50453f79a29c155104b9a7
Olawale <olawale.akinodanye@gmail.com>
2021-04-04 04:49:13 +0100

    first commit

---
+++ App/js/scripts.js
@@ -0,0 +1,18 @@
+const hamburger = document.querySelector(".hero__menu img");
+const mobileMenu = document.querySelector(".mobile__menu");
+
+const btnClickHandler = () => {
+  console.log("working...");
+
+  if (hamburger.classList.contains("change")) {
+    hamburger.classList.toggle("change");
+    hamburger.setAttribute("src", "../../images/icon-hamburger.svg");
+    mobileMenu.classList.remove("mobile-active");
+  } else {
+    hamburger.classList.toggle("change");
+    hamburger.setAttribute("src", "../../images/icon-close.svg");
+    mobileMenu.classList.add("mobile-active");
+  }
+};
+
+hamburger.addEventListener("click", () => btnClickHandler());
